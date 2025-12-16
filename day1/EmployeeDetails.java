30f40dd9ed991eb17a2587bbcc7084b93e73b61e
saranyavedachalam2787 <saranyavedachalam2787@gmail.com>
2025-12-16 23:23:29 +0530

    Week1.day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String name = "John Doe";
+		int age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		int luckyNumber = 7;
+		System.out.println("Name : " + name);
+		System.out.println("age : " + age);
+		System.out.println("salary : " + salary);
+		System.out.println("gender : " + gender);
+		System.out.println("acceptTerms : " + acceptTerms);
+		System.out.println("rating : " + rating);
+		System.out.println("mobileNumbr : " + mobileNumber);
+		System.out.println("pinCode : " + pinCode);
+		System.out.println("luckyNumber :" + luckyNumber);
+	}
+
+}
